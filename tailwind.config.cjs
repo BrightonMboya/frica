1a10d1f539a6e3495e6db47e9e92c0aa421bdbff
BrightonMboya <b.mboya@alustudent.com>
2023-02-18 13:26:50 +0200

    initial draft for Elimino

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,9 @@ module.exports = {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      backgroundImage: {
+        greenBlur: "url('/public/images/greenBlur.png')",
+      },
     },
   },
   plugins: [],
